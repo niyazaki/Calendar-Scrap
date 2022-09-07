c12cdddeb1f5cfba1a8dcdb1323f957e90d111f7
Niyazaki <zakkassabeh@hotmail.com>
2022-09-08 01:53:58 +0200

    Correct cursus Get all the courses

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://hplanning2022.umons.ac.be/invite")
 driver.implicitly_wait(5)
 
-
-#searchField = driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit")
-# print(searchField)
+# Formations
 driver.find_element(
     By.XPATH, '//*[@id="GInterface.Instances[0].Instances[1]_Combo0"]').click()
+# Saisie du nom
 driver.find_element(
     By.XPATH, '//*[@id="GInterface.Instances[1].Instances[1].bouton"]/div/div').click()
+# Selection du cursus
 driver.find_element(
-    By.XPATH, '//*[@id="GInterface.Instances[1].Instances[1]_20"]').click()
+    By.XPATH, '//*[@id="GInterface.Instances[1].Instances[1]_21"]').click()
 
 
 trXpath = '//*[@id="GInterface.Instances[1].Instances[3]_Calendrier"]/table[1]/tbody/tr'
@@ -31,12 +31,11 @@ for i in range(1, 53):
     driver.find_element(By.XPATH, f"{trXpath}/td[{i}]").click()
     week = driver.find_element(
         By.XPATH, '//*[@id="GInterface.Instances[1].Instances[7]_Grille_Elements"]')
+    time.sleep(0.01)
+    weeks[i] = week.get_attribute("innerHTML")
 
-    weeks[i] = week.get_attribute('innerHTML')
-    time.sleep(0.1)
-
-
-print(weeks)
+with open("weeks.txt", "w", encoding="UTF-8", newline="\n") as file:
+    file.write(str(weeks))
 
 
 #iframe = driver.switch_to.frame("GInterface.Instances")
